398600b1c1e8b03d415e7e0d91166cb18e0d744c
Jonghwa <whdghk3471@gmail.com>
2025-04-13 16:44:21 +0900

    #3.1 Connecting the Store

---
+++ src/store.js
@@ -0,0 +1,33 @@
+import { createStore } from "redux";
+
+const ADD = "ADD";
+const DELETE = "DELETE";
+
+export const addToDo = (text) => {
+  return {
+    type: ADD,
+    text,
+  };
+};
+
+export const deleteToDo = (id) => {
+  return {
+    type: DELETE,
+    id,
+  };
+};
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [...state, { id: Date.now(), text: action.text }];
+    case DELETE:
+      return state.filter((toDo) => toDo.id !== action.id);
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+export default store;
